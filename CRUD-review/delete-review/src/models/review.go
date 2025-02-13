3e3e02d204b08a240a05f7205a627fb55346b17c
Diego232323A <dachicaizaq@uce.edu.ec>
2025-02-13 01:31:59 -0500

    refactor: :fire: delete CRUD-review

--- CRUD-review/delete-review/src/models/review.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// Review representa la estructura de una reseña en la BD
-type Review struct {
-	ID      int    `json:"id"`
-	UserID  string `json:"user_id"`
-	Rating  int    `json:"rating"`
-	Comment string `json:"comment"`
-}
